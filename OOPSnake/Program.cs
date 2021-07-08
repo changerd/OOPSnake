1d2fa75498477fd4434425663dee0b98731e2c32
Slava Morza <bloodreek@gmail.com>
2021-07-08 22:03:33 +0300

    Add function Draw and 2 points

--- OOPSnake/Program.cs
+++ OOPSnake/Program.cs
@@ -10,6 +10,25 @@ namespace OOPSnake
     {
         static void Main(string[] args)
         {
+            int x1 = 3;
+            int y1 = 5;
+            char symbol1 = '$';
+
+            Draw(x1, y1, symbol1);
+
+            int x2 = 5;
+            int y2 = 10;
+            char symbol2 = '#';
+
+            Draw(x2, y2, symbol2);
+
+            Console.ReadKey();
+        }
+
+        static void Draw(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
         }
     }
 }
